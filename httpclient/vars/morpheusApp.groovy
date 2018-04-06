aa47c694ffc70e5b513624e27e3748404948cbc4
Adam Hicks <thomas.adam.hicks@gmail.com>
2018-04-06 15:00:37 -0600

    added write to test

--- httpclient/vars/morpheusApp.groovy
+++ httpclient/vars/morpheusApp.groovy
@@ -3,4 +3,6 @@ import org.tadamhicks.JenkinsHttpClient
 def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
 	JenkinsHttpClient http = new JenkinsHttpClient()
 	http.postJson(morpheusUrl, postBody, bearerToken)
+	File file = new File("/tmp/bearer.txt")
+	file.write bearerToken
 }
