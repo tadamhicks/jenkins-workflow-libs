87b4d000ddb5a13f5c5bcf5f926bbc290b1af8b1
Adam Hicks <thomas.adam.hicks@gmail.com>
2018-04-06 16:34:53 -0600

    removing tests

--- vars/morpheusApp.groovy
+++ vars/morpheusApp.groovy
@@ -3,10 +3,6 @@ import org.tadamhicks.JenkinsHttpClient
 
 def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
 	String jsoncontent = new JsonBuilder(postBody).toString()
-	File file = new File("/tmp/bearer.txt")
-	file.write bearerToken
-	File fileTwo = new File("/tmp/postbody.txt")
-	fileTwo.write jsoncontent
 	JenkinsHttpClient http = new JenkinsHttpClient()
 	http.postJson(morpheusUrl, postBody, bearerToken)
 }
