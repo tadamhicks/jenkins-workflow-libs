9905a320cfc3675124ccd377258eeb975be2d0a3
Adam Hicks <thomas.adam.hicks@gmail.com>
2018-04-06 15:16:55 -0600

    error handling

--- vars/morpheusApp.groovy
+++ vars/morpheusApp.groovy
@@ -1,8 +1,12 @@
+import groovy.json.JsonBuilder
 import org.tadamhicks.JenkinsHttpClient
 
 def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
+	String jsoncontent = new JsonBuilder(postBody).toString()
 	File file = new File("/tmp/bearer.txt")
 	file.write bearerToken
+	File file = new File("/tmp/postbody.txt")
+	file.write jsoncontent
 	JenkinsHttpClient http = new JenkinsHttpClient()
 	http.postJson(morpheusUrl, postBody, bearerToken)
 }
