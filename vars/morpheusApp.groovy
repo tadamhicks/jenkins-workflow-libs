d58a7554c4738b2014d966ff3b4a2856734f95c7
Adam Hicks <thomas.adam.hicks@gmail.com>
2018-04-06 15:07:08 -0600

    new

--- vars/morpheusApp.groovy
+++ vars/morpheusApp.groovy
@@ -1,8 +1,8 @@
 import org.tadamhicks.JenkinsHttpClient
 
 def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
-	JenkinsHttpClient http = new JenkinsHttpClient()
-	http.postJson(morpheusUrl, postBody, bearerToken)
 	File file = new File("/tmp/bearer.txt")
 	file.write bearerToken
+	JenkinsHttpClient http = new JenkinsHttpClient()
+	http.postJson(morpheusUrl, postBody, bearerToken)
 }
